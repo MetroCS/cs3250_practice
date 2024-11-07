a8c7d01082fec4e605a958ceca6a14ac692081ce
trklenk01 <thomasklenk@gmail.com>
2024-11-06 19:54:15 -0700

    Class to take criteria

---
+++ decision_support/src/metrocs/defineCriteria/DefineCriteria.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class DefineCriteria here.
+ *
+ * @Thomas (your name)
+ * @1.0 (a version number or a date)
+ */
+public class DefineCriteria
+{
+    // instance variables - replace the example below with your own
+    private int weight = 0;
+
+    /**
+     * Constructor for objects of class DefineCriteria
+     */
+    public DefineCriteria(int critWeight)
+    {
+        weight = critWeight;
+        
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getName()
+    {
+        ;
+    }
+}
